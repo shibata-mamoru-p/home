f65ca95b64f968cb9fad4b466bc8feeda814b521
shibata-mamoru-p <shibata.mamoru.p@gmail.com>
2019-01-02 17:49:55 +0900

    Improve: Modularized ps4 concerns

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
 'use strict'
 
 const mqtt = require('mqtt')
-const {Device} = require('ps4-waker')
+const Ps4 = require('./lib/ps4')
 const Homebridge = require('./lib/homebridge')
 
 const CHANNEL_TOKEN = process.env.BEEBOTTE_CHANNEL_TOKEN_HOME
-const TITLE_ID_TORNE = 'CUSA00442'
 
 const client = mqtt.connect('mqtt://mqtt.beebotte.com',
   {username: `token:${CHANNEL_TOKEN}`, password: ''}
 )
 
 const homebridge = new Homebridge()
+const ps4 = new Ps4()
 
 client.on('message', function (topic, message) {
   let phrase = JSON.parse(message).data
 
   switch (topic) {
     case 'home/ps4':
-      var ps4 = new Device()
-
       if (/つけ|オン/.test(phrase)) {
         ps4.turnOn()
-          .then(() => ps4.close())
           .catch((e) => console.log(e))
       } else if (/消し|けし|オフ/.test(phrase)) {
         ps4.turnOff()
-          .then(() => ps4.close())
           .catch((e) => console.log(e))
       }
 
       break
     case 'home/torne':
-      var ps4 = new Device()
-
       if (/つけ|オン/.test(phrase)) {
-        ps4.startTitle(TITLE_ID_TORNE)
-          .then(() => ps4.close())
+        ps4.startTorne()
           .catch((e) => console.log(e))
       } else if (/消し|けし|オフ/.test(phrase)) {
         ps4.turnOff()
-          .then(() => ps4.close())
           .catch((e) => console.log(e))
       }
 
